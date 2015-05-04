e9ceb713fa31c13cc60e2685c33a87aee8632ca7
Yatin Mistry <javayatin@gmail.com>
2015-05-04 20:08:29 +0200

    Added the servlet config values as annotated. There is no servlet context annotation

--- src/main/java/com/yatin/servlet/HelloWorldServlet.java
+++ src/main/java/com/yatin/servlet/HelloWorldServlet.java
@@ -4,27 +4,36 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/hello", 
+			loadOnStartup = 1, 
+			initParams = { 
+				@WebInitParam(name = "first_init_param_name", value = "first_init_param_value"),
+				@WebInitParam(name = "second_init_param_name", value = "second_init_param_value")
+			})
 public class HelloWorldServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 8971101587096045191L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		String servletContextParam = req.getServletContext().getInitParameter("first_context_param_name");
-		String servletConfigParam = getInitParameter("first_init_param_name");
-		
+		String servletConfigParamOne = getInitParameter("first_init_param_name");
+		String servletConfigParamTwo = getInitParameter("second_init_param_name");
+
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
-		
+
 		out.print("<html><body>");
 		out.println("<b>Hello World</b>");
-		out.println("<b>The servlet Context Param value : " + servletContextParam + "</b>");
-		out.println("<b>The servlet Config param value : " + servletConfigParam + "</b>");
+		out.println("<p>The servlet Context Param value : " + servletContextParam + "</p>");
+		out.println("<p>The servlet Config param values : " + servletConfigParamOne + ", " + servletConfigParamTwo + "</p>");
 		out.print("</body></html>");
 	}
 }
